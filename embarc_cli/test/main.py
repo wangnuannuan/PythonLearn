d767bf5bc500753ae205d5f32166737974d61099
Jingru <jingru@synopsys.com>
2018-12-06 17:03:25 +0800

    travis: modfiy the path of embarc_tools
    Signed-off-by: Jingru <jingru@synopsys.com>

--- embarc_cli/test/main.py
+++ embarc_cli/test/main.py
@@ -13,7 +13,7 @@ def get_allcase():
     return discover
 
 if __name__ == '__main__':
-    COV = coverage.coverage(branch=True, include='./embarc_tools/*')
+    COV = coverage.coverage(branch=True, include='../embarc_tools/*')
     COV.start()
     testfilepath = "test.html"
     ftp = open(testfilepath,'wb')
