bc5226334d60cf1cca2aec40c85270097aebcabc
Jingru Wang <jingru@synopsys.com>
2019-02-19 15:52:40 +0800

    travis: update code ,and use beautifulsoup4
    Signed-off-by: Jingru Wang <jingru@synopsys.com>

--- embarc_cli/embarc_tools/main.py
+++ embarc_cli/embarc_tools/main.py
@@ -5,7 +5,6 @@ import sys
 import pkgutil
 import importlib
 import pkg_resources
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from embarc_tools import commands
 from embarc_tools.commands import config_commands
 
